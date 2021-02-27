abd932e18918b0fa68705a6e94d6259e4636d1cf
chemazu <chukwuemekachemazu@gmail.com>
2021-02-27 17:11:08 +0100

    started the server and connected the mongoose database

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongURI:
+    "mongodb+srv://chemazu:Ratface_3@cluster0.am9d5.mongodb.net/<dbname>?retryWrites=true&w=majority",
+};
---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+const db = require("./config/key").mongURI;
+
+//CONNECTING MONGOOSE
+
+mongoose
+  .connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => console.log("mongodb running"))
+  .catch((err) => console.log("the error is", err));
+
+//STARTING THE SERVER
+app.listen(PORT, console.log(`server started on port ${PORT}`));
